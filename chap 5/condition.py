e1197623dcbfcb900a561d1a14228fc2b681f117
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-06-23 23:41:06 +0500

    Add files via upload

---
+++ chap 5/condition.py
@@ -0,0 +1,21 @@
+# if elif else ladder
+
+a=int(input("Enter your age"))
+
+if(a>=18):
+    print("You are eligible")
+
+
+elif(a<0):
+    print("Invalid negative age")  
+
+
+elif(a==0):
+    print("Invalid age")   
+
+
+else:
+    print("You are not eligible")    
+
+
+print("End of program")
---
+++ chap 5/listname.py
@@ -0,0 +1,6 @@
+l=["Yazdaan","Ali","jia","Zia"]
+name = input("Enter Your Name:")
+if(name in l):
+ print("Your name is in the list")
+else:
+ print("Your name is not in the list")
