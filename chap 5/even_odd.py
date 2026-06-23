307923bfd4b3c5efae23acbcbe115e275c99e4ad
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-06-23 23:41:47 +0500

    Delete chap 5 directory

--- chap 5/even_odd.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Enter any number"))
-if(a%2==0):
-    print(a," is an even number")
-
-else:
-   print(a,"is odd an number")
--- chap 5/listname.py
+++
@@ -1,6 +0,0 @@
-l=["Yazdaan","Ali","jia","Zia"]
-name = input("Enter Your Name:")
-if(name in l):
- print("Your name is in the list")
-else:
- print("Your name is not in the list")
